3aa65752c8e983e61b5859df600dd53c9e3db5e3
Bef-Tiger <55175336+Bef-Tiger@users.noreply.github.com>
2019-10-09 23:32:06 -0700

    Modified methods

--- cse360/src/cse360assign2/AddingMachine.java
+++ cse360/src/cse360assign2/AddingMachine.java
@@ -1,27 +1,45 @@
+/**
+ * @author 		Ulises Chavez Escalante
+ * Class ID 	CSE360-85141
+ * assign2
+ * 
+ */
 package cse360assign2;
+
+import java.util.Scanner;
 
 public class AddingMachine {
 
 	private int total;
 	
 	public AddingMachine () {
-		total = 0;  // not needed - included for clarity
+		total = 0;  
+	}
+	
+	public AddingMachine(int total) {
+		this.total = total;
 	}
 	
 	public int getTotal () {
-		return 0;
+		return this.total;
 	}
 	
 	public void add (int value) {
-		
+		total = total + value;
 	}
 	
 	public void subtract (int value) {
-		
+		total = total - value;
 	}
 		
 	public String toString () {
-		return "";
+		String s = "0 ";
+		do {
+		s = s  + Integer.toString(total);
+		}
+		while(total == 0);
+		
+		return s;
 	}
 
 	public void clear() {
